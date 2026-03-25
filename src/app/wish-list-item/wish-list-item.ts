e39c9787f48a7e5e040923d014f0e4371f8be6ac
DawnHavoc <sarnavadas2@gmail.com>
2026-03-25 14:14:48 +0530

    added component wish-list-item

---
+++ src/app/wish-list-item/wish-list-item.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wish-list-item',
+  imports: [],
+  templateUrl: './wish-list-item.html',
+  styleUrl: './wish-list-item.css',
+})
+export class WishListItem {}
--- src/app/wish-list/wish-list.ts
+++ src/app/wish-list/wish-list.ts
@@ -1,9 +1,10 @@
 import { Component,input } from '@angular/core';
 import { WishItem } from '../../shared/models/wishitem';
+import {WishListItem} from '../wish-list-item/wish-list-item';
 
 @Component({
   selector: 'wish-list',
-  imports: [],
+  imports: [WishListItem],
   standalone:true,
   templateUrl: './wish-list.html',  
   styleUrl: './wish-list.css',
